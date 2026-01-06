2a46d7a1bf57898a4ec126909c3d8e6eaf319307
Lucas Paiva <lpaiva234@gmail.com>
2026-01-05 22:31:33 -0300

    write tests for capitalize function

--- capitalize.test.js
+++ capitalize.test.js
@@ -1,1 +1,17 @@
 import { capitalize } from "./capitalize.js";
+
+test("capitalize first letter", () => {
+    expect(capitalize("hello world")).toBe("Hello world");
+});
+
+test("works with first letter capitalized", () => {
+    expect(capitalize("Hello world")).toBe("Hello world");
+});
+
+test("works with empty string", () => {
+    expect(capitalize("")).toBe("");
+});
+
+test("works with unitary string", () => {
+    expect(capitalize("a")).toBe("A");
+});
